e489652d194241aa3929c8f8d39cfc923397f0b0
maguibing <kuse012@163.com>
2022-06-28 17:38:20 +0800

    finish guide

--- src/index.js
+++ src/index.js
@@ -1,15 +1,21 @@
 import _ from 'lodash'
-// import Print from './print'
-import Print from './print'
-
 function component() {
   const element = document.createElement('div')
+  const button = document.createElement('button')
+  const br = document.createElement('br')
 
+  button.innerHTML = 'Click me and look at the console!'
   element.innerHTML = _.join(['Hello', 'webpack'], ' ')
-  //   element.onclick = Print.bind(null, 'Hello webpack!')
-  element.onclick = Print.bind(null, 'Hello webpack!')
+  element.appendChild(br)
+  element.appendChild(button)
+
+  button.onclick = (e) =>
+    import(/* webpackChunkName: "print" */ './print').then((module) => {
+      const print = module.default
+
+      print()
+    })
 
   return element
 }
-
 document.body.appendChild(component())
--- webpack.common.js
+++ webpack.common.js
@@ -4,12 +4,12 @@ const HtmlWebpackPlugin = require('html-webpack-plugin')
 // https://stackoverflow.com/questions/56567930/typeerror-cleanwebpackplugin-is-not-a-constructor
 const { CleanWebpackPlugin } = require('clean-webpack-plugin')
 module.exports = {
-  mode: 'development',
+  //   mode: process.env.NODE_ENV,
 
-  // 入口文件
+  // 入口文件 可以有多个入口文件
   entry: {
     // shared: 'lodash',
-    index: './src/index.js',
+    app: './src/index.js',
     // print: './src/print.js',
     // index: {
     //   import: './src/index.js',
@@ -22,33 +22,33 @@ module.exports = {
   },
 
   // 生产环境不建议使用
-  devtool: 'inline-source-map',
-  devServer: {
-    static: './dist',
-  },
+  //   devtool: 'inline-source-map',
+  //   devServer: {
+  //     static: './dist',
+  //   },
 
   // optimization 优化 因为在这个示例中单个 HTML 页面有多个入口，所以添加了 optimization.runtimeChunk: 'single' 配置.
-  optimization: {
-    runtimeChunk: 'single',
-    moduleIds: 'deterministic',
-    splitChunks: {
-      //   include all types of chunks
-      //   chunks: 'all',
-      cacheGroups: {
-        vendor: {
-          test: /[\\/]node_modules[\\/]/,
-          name: 'vendors',
-          chunks: 'all',
-        },
-      },
-    },
-  },
-
+  //   optimization: {
+  //     runtimeChunk: 'single',
+  //     moduleIds: 'deterministic',
+  //     splitChunks: {
+  //       //   include all types of chunks
+  //       //   chunks: 'all',
+  //       cacheGroups: {
+  //         vendor: {
+  //           test: /[\\/]node_modules[\\/]/,
+  //           name: 'vendors',
+  //           chunks: 'all',
+  //         },
+  //       },
+  //     },
+  //   },
+  // output 只能有一个出口文件， 并且需要是绝对路径
   output: {
-    filename: '[name].[contenthash].js',
+    filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'dist'),
     clean: true,
-    publicPath: '/',
+    // publicPath: '/',
   },
 
   plugins: [
